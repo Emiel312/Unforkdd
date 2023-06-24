1b37b35935b849e7bb50cb4e2ed1a2fcaa13d694
jsimonis <js@appsforce.org>
2023-06-24 11:11:44 +0200

    login/registratie layout fixes

--- Unforkdd/LoginPagina.xaml.cs
+++ Unforkdd/LoginPagina.xaml.cs
@@ -1,38 +1,38 @@
 namespace Unforkdd;
-
+using Microsoft.Maui.Graphics;
 public partial class LoginPagina : ContentPage
 {
-	public LoginPagina()
-	{
-		InitializeComponent();
-	}
-	private void OnLoginClicked(object sender, EventArgs e)
-	{
-		string username = UsernameEntry.Text;
-		string password = PasswordEntry.Text;
+    public LoginPagina()
+    {
+        InitializeComponent();
+    }
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+        string username = UsernameEntry.Text;
+        string password = PasswordEntry.Text;
 
-		if (isValidCredentials(username, password)) 
-		{
-			Navigation.PushAsync(new ActiviteitenPagina());
-		}
-		else
-		{
-			DisplayAlert("Error", "Invalid username or password.", "OK");
-		}
-	}
-	private void OnForgotPasswordClicked(object sender, EventArgs e)
-	{
+        if (isValidCredentials(username, password))
+        {
+            Navigation.PushAsync(new ActiviteitenPagina());
+        }
+        else
+        {
+            DisplayAlert("Error", "Invalid username or password.", "OK");
+        }
+    }
+    private void OnForgotPasswordClicked(object sender, EventArgs e)
+    {
         //DisplayAlert("Forogot Password", "Please contact support to reset your password", "OK");
         Navigation.PushAsync(new WachtwoordResetPagina());
     }
-	private void OnRegisterClicked(object sender, EventArgs e)
-	{
+    private void OnRegisterClicked(object sender, EventArgs e)
+    {
         Navigation.PushAsync(new RegistratiePagina());
         //DisplayAlert("Register", "Redirecting to registration page", "OK");
-	}
-	private bool isValidCredentials(string username, string password) 
-	{
+    }
+    private bool isValidCredentials(string username, string password)
+    {
         // Test logica
-		return (username == "admin" && password == "password");
+        return (username == "admin" && password == "password");
     }
 }
